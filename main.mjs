973955013bba121989cc0d1d8cac4bfdfeab9792
Ratstar216 <hosi7tm@gmail.com>
2024-07-02 18:38:31 +0900

    return random message

--- main.mjs
+++ main.mjs
@@ -3,6 +3,16 @@ import express from "express";
 import { PrismaClient } from "@prisma/client";
 import escapeHTML from "escape-html";
 
+const responseList = ["Hello", "Hi", "Hey", "Good morning", "Good evening"]; 
+
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("static"));
@@ -21,6 +31,9 @@ app.get("/", async (request, response) => {
 app.post("/send", async (request, response) => {
   await prisma.post.create({
     data: { message: request.body.message },
+  });
+  await prisma.post.create({
+    data: { message: responseList[getRandomIntInclusive(0, responseList.length - 1)]},
   });
   response.redirect("/");
 });
